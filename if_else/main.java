9956a9138a74fc9e29b437abf4856a21b598007f
Soham Mondal <sohammondal81@gmail.com>
2022-10-30 17:55:35 +0530

    if and else statements

---
+++ if_else/main.java
@@ -0,0 +1,10 @@
+public class main {
+	public static void main(String args[]){
+		int age = 22;
+		if(age < 20){
+			System.out.println("Not greater than 20. ");
+		}else {
+			System.out.println("Greater than 20. ");
+		}
+	}
+}
